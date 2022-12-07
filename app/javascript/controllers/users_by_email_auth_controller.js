6752ba8ae201dc517a00fee87ec785c3fc466a94
Duncan Murchison <duncancmurchison@gmail.com>
2022-12-07 04:09:41 -0500

    modal login vs signup logic

--- app/javascript/controllers/users_by_email_auth_controller.js
+++ app/javascript/controllers/users_by_email_auth_controller.js
@@ -1,9 +1,11 @@
 import { Controller } from '@hotwired/stimulus';
+import axios from 'axios';
 
 export default class extends Controller {
   static targets = ['email', 'submit'];
 
   connect() {
+    console.log('axios: ', axios);
     this.submitTarget.addEventListener('click', (e) => {
       e.preventDefault();
 
@@ -11,6 +13,18 @@ export default class extends Controller {
         // email field is empty, so don't do anything
       } else {
         // email field is filled out, so do something
+        axios.get('/api/users_by_email', {
+          params: {
+            email: this.emailTarget.value
+          },
+          headers: {
+            'ACCEPT': 'application/json'
+          }
+        }).then((response) => {
+          Turbo.visit('/users/sign_in')
+        }).catch((response) => {
+          Turbo.visit('/users/sign_up')
+        })
       }
     });
   }
