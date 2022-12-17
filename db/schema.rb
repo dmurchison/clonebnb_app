f5e5e66e09ad79e8452944ca5f598d94d7f91641
Duncan Murchison <duncancmurchison@gmail.com>
2022-12-16 23:35:57 -0500

    property spec

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_13_155329) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_17_043215) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "properties", force: :cascade do |t|
     t.string "name"
-    t.string "headling"
+    t.string "headline"
     t.text "description"
     t.string "city"
     t.string "state"
